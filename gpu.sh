f85f85b4286aed38641ce8cb8a64b6a854824749
Mwalusakaa of Zed <146742130+Mwalusakaa@users.noreply.github.com>
2023-10-02 19:22:28 +0100

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -15,7 +15,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xC63153AB2D4e0fcC2ee66C78fE6747561D96c3a0/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
